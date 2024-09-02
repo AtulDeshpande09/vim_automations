ad95e71ebd222b7abb1195cee914e8da2959ae0d
Atul Deshpande <159980686+AtulDeshpande09@users.noreply.github.com>
2024-09-02 07:32:01 +0530

    Update boiler_plate.py
    added boilerplates for c , c# and css

--- boilerplates/boiler_plate.py
+++ boilerplates/boiler_plate.py
@@ -43,6 +43,43 @@ int main() {
     return 0;
 }"""
 
+def generate_c_template():
+    return "#include <stdio.h>
+
+int main(void) {
+    // This is the main function of the program
+    printf("Hello, World!\n");
+
+    return 0;
+}"
+
+def generate_cSharp_template():
+    return "namespace MyProject;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}"
+
+def generate_css_template():
+    return "*{
+margin: 10px;
+
+padding: 10px;
+
+box-sizing: border-box;
+}
+
+html {
+height: 100%;
+}
+body {
+min-height: 100%;
+background-color:black;
+}"
 
 
 
@@ -52,10 +89,21 @@ if __name__=="__main__":
 
         if boilerplate_type == "cpp":
             print(generate_cpp_template())
+            
         elif boilerplate_type=="java":
             print(generate_java_template())
+            
         elif boilerplate_type =="html":
             print(generate_html_template())
+            
+        elif boilerplate_type=="csharp":
+            print(generate_cSharp_template())
+            
+        elif boilerplate_type =="c":
+            print(generate_c_template())
+        elif boilerplate_type =="css":
+            print(generate_css_template())
+            
         else:
             print("Look what are you typing fella!")
 
